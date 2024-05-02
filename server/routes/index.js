eeed1b900e36526b7eb9e163fe8aeb3230f4ca49
MargotDem <margot.demaulmont@gmail.com>
2024-05-02 14:30:55 +0300

    deleted some files

--- server/routes/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.plantsRouter = exports.usersRouter = void 0;
-var users_1 = require("./users");
-Object.defineProperty(exports, "usersRouter", { enumerable: true, get: function () { return users_1.usersRouter; } });
-var plants_1 = require("./plants");
-Object.defineProperty(exports, "plantsRouter", { enumerable: true, get: function () { return plants_1.plantsRouter; } });
